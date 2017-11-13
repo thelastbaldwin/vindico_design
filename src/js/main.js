26f5e3da6cdcb5b961f03ac53450a232f4a292d4
Steve Minor <stv.mnr@gmail.com>
2017-11-13 07:47:24 -0800

    completely remove skeleton.css. Got all animations hooked up

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 $(()=>{
-  $(".portfolio-toggle").click((event)=>{
-  	const $button = $(event.target);
+  $(".portfolio-toggle").click(function(){
+  	const $button = $(this);
 
     $button.toggleClass("active");
-    console.log($button.hasClass("active") ? $button.text("see less") : $button.text("see more"));
+    $button.hasClass("active") ? $button.text("see less") : $button.text("see more");
     $(".more-portfolio").slideToggle("fast");
+  });
+
+  $(".copy-toggle").click(function(){
+    const $this = $(this);
+
+    $this.prev().slideToggle("fast");
+    $this.toggleClass("active");
   })
 });
